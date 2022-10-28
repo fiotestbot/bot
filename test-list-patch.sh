36d07cf61a60a9679a3381bb802624d817a79846
Vincent Fu <vincent.fu@samsung.com>
2022-10-27 22:51:42 -0400

    test-list-patch: eliminate temp file
    We no longer need a temporary file using the trick from:
    
    https://josefbacik.github.io/kernel/2021/10/18/lei-and-b4.html
    
    Signed-off-by: Vincent Fu <vincent.fu@samsung.com>

--- test-list-patch.sh
+++ test-list-patch.sh
@@ -13,13 +13,10 @@ fi
 BRANCH="$1"
 REF="$2"
 DIR=fio-${BRANCH}
-TEMP_FILE=$(mktemp)
 
 rm -rf ${DIR}
 git clone ssh://vincentkfu@git.kernel.dk/data/git/fio.git ${DIR}
-b4 am ${REF} &> ${TEMP_FILE} || abort
-MBX=$(tail ${TEMP_FILE} -n 1 | tr -s " " | cut -d " " -f 4)
 cd ${DIR}
-git am -s --whitespace=fix .${MBX}
+b4 am -o - ${REF} | git am -s --whitespace=fix
 git checkout -b ${BRANCH}
 git push git@github.com:vincentkfu/fio.git ${BRANCH}
